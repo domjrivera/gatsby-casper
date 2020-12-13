db954af83317bc77b81c6151615fea07490c06b3
Domingo Rivera <djr@infosecusa.com>
2020-12-12 22:54:25 -0500

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,20 +49,20 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
-  coverImage: 'img/blog-cover.png',
+  title: 'Cyber Lawyer Tech Cases By Domingo Rivera',
+  description: 'Internet Law Cases By Domingo Rivera',
+  coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
-  showSubscribe: true,
+  siteUrl: 'https://www.cyberlawyer.tech',
+  facebook: 'https://www.facebook.com/avmtech',
+  twitter: 'https://twitter.com/cyberforensix',
+  showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: 'is based on Gatsby Casper',
+  footer: 'by Domingo J. Rivera',
 };
 
 export default config;
